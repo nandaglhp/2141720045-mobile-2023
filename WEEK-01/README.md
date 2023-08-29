13a389f86ba640ed5f0b9dd5e20590cd488acb79
= <=>
2023-08-29 16:30:22 +0700

    first commit

---
+++ WEEK-01/README.md
@@ -0,0 +1,13 @@
+# Tugas Pertemuan Pertama Mobile Programming
+
+Nama : Ananda Galih Pratiwi
+
+Kelas : TI - 3F
+
+Absen : 08
+
+NIM : 2141720045
+
+# Lampiran Gambar
+
+![Screenshot hello_world](docs/hello_world.png)
---
+++ WEEK-01/src/hello_world.dart
@@ -0,0 +1,8 @@
+void main() {
+  print("Hello World!");
+  print("This is my first dart program. by : ");
+  print("Name  : Ananda Galih Pratiwi");
+  print("Kelas : TI - 3F");
+  print("Absen : 5");
+  print("NIM   : 2141720045");
+}
